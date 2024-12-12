101da43bb7b5e87ced893c1d5cb9e709ce54a8ae
marko <markomarynovych@gmail.com>
2024-12-12 14:29:39 +0200

    corses

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-import { setupCors } from './setup-cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {});
@@ -21,7 +20,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('docs', app, document);
 
-  setupCors(app);
   await app.listen(port);
 }
 bootstrap();
